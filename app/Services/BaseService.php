24982a6b886b541f0e2f547cfb2d38a80c6fe1bc
Felipe Braiani <felipe.braianitech@gmail.com>
2021-11-05 23:05:48 -0400

    Features to BaseService added

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -5,4 +5,16 @@ namespace App\Services;
 class BaseService
 {
 
+    public function messageReturn($resource, $action)
+    {
+        $availableMessages = [
+            'created' => 'The :resource was created!',
+            'deleted' => 'The :resource was deleted!',
+            'restored' => 'The :resource was restored!',
+            'updated' => 'The :resource was updated!',
+            'ran' => 'The action ran successfully!',
+        ];
+
+        return trans($availableMessages[$action], ['resource' => $resource]);
+    }
 }
